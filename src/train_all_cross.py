ddbdc8e06c32b0b97a3879b9cce1bbbcbb59951e
Han Hoang <gihoang@ucsd.edu>
2025-02-05 23:36:51 -0800

    update train code, add benchmarking result for baseline model, and trained Pytorch baseline model

--- src/train_all_cross.py
+++ src/train_all_cross.py
@@ -15,15 +15,17 @@ from torch_geometric.nn import global_mean_pool, global_max_pool, global_add_poo
 from torch_geometric.utils import scatter
 
 import time
+from datetime import datetime
 import wandb
 from tqdm import tqdm
 from collections import Counter
 
 import sys
-sys.path.insert(1, 'data/')
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))  # Add the parent directory to the path
+sys.path.append("../data/")
+sys.path.append("../models/")
+sys.path.append("../models/layers/")
 from pyg_dataset import NetlistDataset
-
-sys.path.append("models/layers/")
 from models.model_att import GNN_node
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
@@ -42,9 +44,9 @@ def compute_metrics(true_labels, predicted_labels):
     return accuracy, precision, recall
 
 ### hyperparameter ###
-test = False # if only test but not train
+test = True # if only test but not train
 restart = False # if restart training
-reload_dataset = False # if reload already processed h_dataset
+reload_dataset = True # if reload already processed h_dataset
 
 if test:
     restart = True
@@ -55,11 +57,23 @@ num_dim = 32 #large number will cause OOM
 vn = True #use virtual node or not
 trans = False #use transformer or not
 aggr = "add" #use aggregation as one of ["add", "max"]
-device = "cpu" #use cuda or cpu
+device = "cuda" #use cuda or cpu
 learning_rate = 0.001
 
+parent_directory = os.path.dirname(os.path.abspath(__file__))  # Parent directory of the current script
+data_directory = os.path.join(parent_directory, '..', 'data')
+data_file_path = os.path.join(data_directory, 'h_dataset.pt')
+model_directory = os.path.join(parent_directory, '..', 'models/trained_models')
+model_file_path = os.path.join(model_directory, f"{model_type}_{num_layer}_{num_dim}_{vn}_{trans}_model.pt")
+result_directory = os.path.join(parent_directory, '..', 'profiling_results')
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+output_subdirectory = os.path.join(result_directory, current_datetime)  # Subdirectory name based on the current date and time
+# Create the subdirectory if it doesn't exist
+if not os.path.exists(output_subdirectory):
+    os.makedirs(output_subdirectory)
+
 if not reload_dataset:
-    dataset = NetlistDataset(data_dir="data/superblue", load_pe = True, pl = True, processed = True, load_indices=None)
+    dataset = NetlistDataset(data_dir="../data/superblue", load_pe = True, pl = True, processed = True, load_indices=None)
     h_dataset = []
     for data in tqdm(dataset):
         num_instances = data.node_features.shape[0]
@@ -107,19 +121,17 @@ if not reload_dataset:
         h_data['variant_data_lst'] = variant_data_lst
         h_dataset.append(h_data)
         
-    torch.save(h_dataset, "h_dataset.pt")
+    torch.save(h_dataset, data_file_path)
     
 else:
-    dataset = torch.load("h_dataset.pt")
+    dataset = torch.load(data_file_path)
     h_dataset = []
     for data in dataset:
         h_dataset.append(data)
-    
-sys.path.append("models/layers/")
 
 h_data = h_dataset[0]
 if restart:
-    model = torch.load(f"{model_type}_{num_layer}_{num_dim}_{vn}_{trans}_model.pt")
+    model = torch.load(model_file_path)
 else:
     model = GNN_node(num_layer, num_dim, 1, 1, node_dim = h_data['node'].x.shape[1], net_dim = h_data['net'].x.shape[1], gnn_type=model_type, vn=vn, trans=trans, aggr=aggr, JK="Normal").to(device)
 
@@ -127,11 +139,13 @@ criterion_node = nn.MSELoss()
 criterion_net = nn.MSELoss()
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate,  weight_decay=0.01)
 load_data_indices = [idx for idx in range(len(h_dataset))]
-all_train_indices, all_valid_indices, all_test_indices = load_data_indices[:10], load_data_indices[10:], load_data_indices[10:]
+all_train_indices, all_valid_indices, all_test_indices = load_data_indices[:5], load_data_indices[11:], load_data_indices[11:]
 best_total_val = None
 
 if not test:
-    for epoch in range(1):
+    t0 = time.time()
+    
+    for epoch in range(500):
         np.random.shuffle(all_train_indices)
         loss_node_all = 0
         loss_net_all = 0
@@ -184,6 +198,14 @@ if not test:
         if (best_total_val is None) or ((loss_node_all/all_train_idx) < best_total_val):
             best_total_val = loss_node_all/all_train_idx
             torch.save(model, f"{model_type}_{num_layer}_{num_dim}_{vn}_{trans}_model.pt")
+    t1 = time.time()
+    total_time = t1 - t0
+    total_time_in_minutes = total_time / 60
+    print(f"Total runtime in minutes: {total_time_in_minutes}")
+    file_path = os.path.join(output_subdirectory, "total_runtime.txt")
+    with open(file_path, "w") as f:
+        f.write(total_time)
+    print(f"Total runtime result saved to: {file_path}")
 else:
     all_test_idx = 0
     test_loss_node_all = 0
@@ -204,5 +226,14 @@ else:
             test_loss_node_all +=  test_loss_node.item()
             test_loss_net_all += test_loss_net.item()
             all_test_idx += 1
-    print("avg test node demand mse: ", test_loss_node_all/all_test_idx)
-    print("avg test net demand mse: ", test_loss_net_all/all_test_idx)
+
+    avg_test_node_demand_mse = test_loss_node_all/all_test_idx
+    avg_test_net_demand_mse = test_loss_net_all/all_test_idx
+    print("avg test node demand mse: ", avg_test_node_demand_mse)
+    print("avg test net demand mse: ", avg_test_net_demand_mse)
+
+    file_path = os.path.join(output_subdirectory, "test_performance.txt")
+    with open(file_path, "w") as f:
+        f.write(f"Average test MSE for node demand regression: {avg_test_node_demand_mse}\n")
+        f.write(f"Average test MSE for net demand regression: {avg_test_net_demand_mse}")
+    print(f"Test performance result saved to: {file_path}")
